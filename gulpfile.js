c23cf6e57dc34a2f0316a81e008a7b9bd70716d8
Your Name <manabe@ 101de-sign.com>
2018-03-14 22:10:25 +0900

    初めてのgit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+// gulpプラグインの読み込み
+const gulp = require('gulp');
+// Sassをコンパイルするプラグインの読み込み
+const sass = require('gulp-sass');
+ 
+// style.scssをタスクを作成する
+gulp.task('default', function () {
+    // ★ style.scssファイルを監視
+  gulp.watch('src/sass/style.scss', function () {
+    // style.scssの更新があった場合の処理
+
+    // style.scssファイルを取得
+    gulp.src('src/sass/style.scss')
+        // Sassのコンパイルを実行
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        // cssフォルダー以下に保存
+        .pipe(gulp.dest('assets/css'));
+    });
+});
